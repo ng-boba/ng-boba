83d9f4ac84badedf12bbd9c3d2eff5a20bb8908d
Guerric Sloan <guerric.sloan@gmail.com>
2014-07-26 03:29:39 -0700

    Update spec for NGDependencyGraph to reflect rename

--- test/NGDependencyGraph.spec.js
+++ test/NGDependencyGraph.spec.js
@@ -1,8 +1,8 @@
 
-var GDependencyGraph = require('../src/GDependencyGraph');
+var NGDependencyGraph = require('../src/NGDependencyGraph');
 var MockFileObjects = require('./mock/MockFileObjects');
 
-describe('GDependencyGraph', function() {
+describe('NGDependencyGraph', function() {
 	'use strict';
 
 	beforeEach(function () {
@@ -10,24 +10,24 @@ describe('GDependencyGraph', function() {
 	});
 
 	it('creates a graph from dependencies', function() {
-		var g = new GDependencyGraph(MockFileObjects.getDependencies());
+		var g = new NGDependencyGraph(MockFileObjects.getDependencies());
 		expect(g).toBeDefined();
 	});
 
 	it('creates graph nodes from dependencies', function() {
-		var g = new GDependencyGraph(MockFileObjects.getDependencies());
+		var g = new NGDependencyGraph(MockFileObjects.getDependencies());
 		expect(g.nodes.length).toBeGreaterThan(0);
 	});
 
 	it('supports 1-to-1 and links dependencies from file1 to file3', function() {
-		var g = new GDependencyGraph(MockFileObjects.getDependencies());
+		var g = new NGDependencyGraph(MockFileObjects.getDependencies());
 		expect(g.nodes[0].edges.length).toEqual(1);
 		var node = g.nodes[0].edges[0];
 		expect(node.id).toEqual('file2.js');
 	});
 
 	it('generates 1-to-1 file list', function() {
-		var g = new GDependencyGraph(MockFileObjects.getDependencies());
+		var g = new NGDependencyGraph(MockFileObjects.getDependencies());
 		var files = g.getBundleFiles('main');
 		expect(files.length).toEqual(2);
 		expect(files[0]).toEqual('file2.js');
