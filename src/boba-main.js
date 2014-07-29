7d6edd26c2aa1c18164f7dc4762d482e1aebf9c6
Jessica Vreeland <jessicavreeland@gmail.com>
2014-07-28 23:49:13 -0700

    rename config

--- src/boba-main.js
+++ src/boba-main.js
@@ -13,30 +13,29 @@ fs.readFile(file, 'utf8', function (err, data) {
 
 
 var BobaParserTools = require('./BobaParserTools');
-var NGDependencyGraph = require('./NGDependencyGraph');
+var NGDependencyGraph = require('./data/NGDependencyGraph');
 function addBoba(config) {
-	var jsonConfig = config;
-	if (!jsonConfig) {
+	if (!config) {
 		throw ('Boba needs a config to run.');
 	}
 
-	if (!jsonConfig.folder && !jsonConfig.files) {
+	if (!config.folder && !config.files) {
 		throw ('Add files or folder option to your config');
 	}
 
-	if (jsonConfig.folder && jsonConfig.files) {
+	if (config.folder && config.files) {
 		throw ('Both files and folder options cannot be specified.  Choose one.');
 	}
 
-	if (!jsonConfig.modules || jsonConfig.modules.length == 0) {
+	if (!config.modules || config.modules.length == 0) {
 		throw ('Specify one or more angular modules.');
 	}
 
-	if (jsonConfig.folder) {
-		BobaParserTools.parseFolder(jsonConfig.folder).then(function(dependencies) {
+	if (config.folder) {
+		BobaParserTools.parseFolder(config.folder).then(function(dependencies) {
 			
 			var g = new NGDependencyGraph(dependencies, {});
-			var files = g.getBundleFiles(jsonConfig.modules[0]);
+			var files = g.getBundleFiles(config.modules[0]);
 			console.log(files);
 			return files;
 		})
