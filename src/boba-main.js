7193c053018d3254e11f1b2fac2bbc7c6a1f0fb6
Guerric Sloan <guerric.sloan@gmail.com>
2014-08-02 18:57:02 -0700

    Take configuration as arg, support outputting file list to disk

--- src/boba-main.js
+++ src/boba-main.js
@@ -1,21 +1,26 @@
 
+var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs');
-var file = 'boba-config.json';
+var BobaParserTools = require('./parser/BobaParserTools');
+var NGProject = require('./data/NGProject');
 
+// output the argv details
+//console.dir(argv);
+
+var file = argv.config ? argv.config : 'boba-config.json';
+
+// stores all the configuration for ng-boba
+var config;
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
     console.log('Error: ' + err);
     return;
   }
-  var jsonConfig = JSON.parse(data);
-  addBoba(jsonConfig);
-})
+	config = JSON.parse(data);
+  addBoba();
+});
 
-
-var BobaParserTools = require('./parser/BobaParserTools');
-var NGProject = require('./data/NGProject');
-
-function addBoba(config) {
+function addBoba() {
 	if (!config) {
 		throw ('Boba needs a config to run.');
 	}
@@ -32,23 +37,58 @@ function addBoba(config) {
 		throw ('Specify one or more angular modules.');
 	}
 
-	if (config.folder) {
+	if (config.output) {
 
-		BobaParserTools.parseFolder(config.folder).then(function(parsedFiles) {
-
-			var project = new NGProject();
-			parsedFiles.forEach(function(fileObject) {
-				project.addFileComponents(fileObject.filePath, fileObject.results);
-			});
-
-			config.dependencies.forEach(function(dependency) {
-				project.addBaseDependency(dependency);
-			});
-
-			var files = project.getBundleFiles(config.modules[0]);
-			console.log(files);
-			return files;
-		})
-
+		// TODO: validate that the output file is empty
 	}
+
+	if (config.folder) {
+		BobaParserTools.parseFolder(config.folder).then(handleParsedFiles).done();
+	} else if (config.files) {
+		throw 'Not implemented';
+	}
+}
+
+function handleParsedFiles(parsedFiles) {
+	var project = new NGProject();
+	parsedFiles.forEach(function(fileObject) {
+		project.addFileComponents(fileObject.filePath, fileObject.results);
+	});
+
+	config.dependencies.forEach(function(dependency) {
+		project.addBaseDependency(dependency);
+	});
+
+	var files = project.getBundleFiles(config.modules[0]);
+	outputFiles(files);
+}
+
+function outputFiles(files) {
+	var s = JSON.stringify(formatOutput(files));
+
+	if (config.output) {
+
+		// write the file list to file
+		fs.writeFile(config.output, s, function(err) {
+			if (err) {
+				return;
+			}
+		});
+		return;
+	}
+
+	// simple output
+	console.log(files);
+}
+
+/**
+ * Creates the output envelope for the file list
+ * @param {Array} files
+ * @returns {Object}
+ */
+function formatOutput(files) {
+	return {
+		generator: 'ng-boba',
+		files: files
+	};
 }
